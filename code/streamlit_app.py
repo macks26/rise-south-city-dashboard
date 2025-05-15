edc95f5491423fa2dde17181f75093b8406f418c
Isaac Krementsov <isaackrementsov@gmail.com>
2025-05-14 21:03:36 -0700

    Minor changes to streamlit

--- code/streamlit_app.py
+++ code/streamlit_app.py
@@ -100,7 +100,6 @@ with tab1:
     tracts_with_data = gpd.read_file(geojson_path)
 
     # Add in health risk
-    health_risk["geoid"] = "06081" + (health_risk["tract"] * 100).astype(int).astype(str)
     tracts_with_data = tracts_with_data.merge(health_risk, on="geoid")
     tracts_with_data["risk_index"] = health_weight * tracts_with_data["Health Risk Index"] + air_weight * (tracts_with_data["combined_aqi"] / tracts_with_data["combined_aqi"].max())
 
